db6a62f1aaeb88f3906bc005c792a70a6e6acf19
Ken Muse <kenmuse@users.noreply.github.com>
2023-07-19 21:22:35 -0400

    Create Messenger.cs

---
+++ src/Messenger.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Muse.SourceLink.Library
+{
+    public class Messenger
+    {
+        public string GenerateMessage(string source)
+        {
+            return $"Hello world from {source}!";
+        }
+    }
+}
